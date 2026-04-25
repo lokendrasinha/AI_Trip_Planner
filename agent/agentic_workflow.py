17b5b09e11251b6f895d92cf66593cd880d2c620
lokendrasinha <lokendrasinha2003@gmail.com>
2026-04-25 12:18:38 +0530

    Code updated for agent_fuction and system prompt added

--- agent/agentic_workflow.py
+++ agent/agentic_workflow.py
@@ -18,10 +18,15 @@ class GraphBuilder():
             #CalculatorTool()
             #CurrencyConverterTool()
         ]
+        self.system_prompt = SYSTEM_PROMPT
 
 
-    def agent_function(self):
-        pass
+    def agent_function(self,state:MessagesState):
+        """Main agent function"""
+        user_question = state["messages"]
+        input_question = [self.system_prompt] + user_question
+        response = self.llm_with_tools.invoke(input_question)
+        return {"messages": [response]}
 
     
 
@@ -34,6 +39,9 @@ class GraphBuilder():
        graph_builder.add_edge("tools","agent")
        graph_builder.add_edge("agent",END)
 
+       self.graph = graph_builder.compile()
+       return self.graph
+
     
     def __call__(self):
-        pass
+        return self.build_graph()
